114ab177e3a849fe4305df0ac6347e7e1d690fcc
tuanndph16237 <tuanndph16237@example.com>
2022-08-10 07:55:03 +0700

    redux demo with vite

--- web209-redux/src/main.jsx
+++ web209-redux/src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css';
 import App from "./App";
 import "antd/dist/antd.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
--- web209-redux/src/redux/actions.js
+++ web209-redux/src/redux/actions.js
@@ -1,6 +1,6 @@
-// Product
+// Index
 export const GETPRODUCTS = async (dispatch) => {
-    const data = await (await fetch('https://62e09d8bfa8ed271c4855b07.mockapi.io/product')).json()
+    const data = await (await fetch('https://62de615accdf9f7ec2d66ae3.mockapi.io/api/products')).json()
     dispatch({
         type: "product/get",
         payload: data
